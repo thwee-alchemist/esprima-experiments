0b37ae1f88606301a5d5003c49e2b7f6b483fb3c
Joshua Marshall Moore <thwee.abacadabra.alchemist@gmail.com>
2018-07-21 12:16:56 -0700

    second success: a quine, I think

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var esprima = require('esprima');
 var fs = require('fs');
 var program = fs.readFileSync('./index.js').toString();
-var syntax = esprima.parse("var answer = 42;");
+var syntax = esprima.parse(program);
 console.log(syntax);
 
